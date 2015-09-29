a168b33f72c7bd74db2bd8de5f2fbe26a85b4a84
rj@eth3.org <rj@eth3.org>
2015-09-29 19:01:41 -0400

    broadcast to everyone when a user disconnects

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
+    console.log('a user connected');
+    io.emit('CONN', 'a user connected');
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+        io.emit('CONN', 'a user disconnected');
+    });
 });
 
 io.on('connection', function(socket){
